614c6c5b04befc461f693439e1d32b933d01e176
lxj <1073830130@qq.com>
2021-04-29 22:13:30 +0800

    update

--- src/docs/demos/demo1.tsx
+++ src/docs/demos/demo1.tsx
@@ -1,5 +1,24 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useMemo} from 'react';
 import MyModalApi from './my-modal-api';
+import {createRandString} from "@lxjx/utils";
+
+const Test = () => {
+
+  useMemo(() => {
+    console.time('ttt')
+    const a = Array.from({ length: 200000 }).map(() => createRandString(5));
+    console.timeEnd('ttt')
+
+    return a;
+  }, []);
+
+  return (
+    <div>
+      <div>这是model的内容</div>
+      可以放置<strong>任意</strong>内容
+    </div>
+  )
+}
 
 const Demo1 = () => {
   useEffect(() => {
@@ -9,16 +28,15 @@ const Demo1 = () => {
   }, []);
 
   function renderHandle() {
+    console.time('time');
     // 每次render执行会返回一个RenderApiComponentInstance实例对象, 可以管理该实例的各种状态和行为
     MyModalApi.render({
       title: '这是modal的标题',
       content: (
-        <div>
-          <div>这是model的内容</div>
-          可以放置<strong>任意</strong>内容
-        </div>
+        <Test />
       ),
     });
+    console.timeEnd('time');
   }
 
   return (
